6396c56e37e1dd566b3ff7183401ace4127b62ef
Michael Amudeshnikov <83592338+misha99fr@users.noreply.github.com>
2023-05-31 21:33:17 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,28 @@
+local GUI = require("GUI")
+local system = require("System")
+
+local workspace, window, menu = system.addWindow(GUI.filledWindow(1, 1, 50, 16, 0x9966CC))
+
+local layout = window:addChild(GUI.layout(1, 1, window.width, window.height, 1, 1))
+
+local label = layout:addChild(GUI.label(1, 1, 25, 1, 0x0, "Mindows Setup"))
+
+local button1 = layout:addChild(GUI.button(1, 1, 11, 1, 0x4B4B4B, 0xFFFFFF, 0x0, 0xFFFFFF, "1"))
+button1.onTouch = function()
+  window:remove()
+  GUI.alert("АХАХАХАХХАХАХАХ, хер тебе а не Mindows 10")
+end
+
+local button2 = layout:addChild(GUI.button(1, 1, 8, 1, 0x4B4B4B, 0xFFFFFF, 0x0, 0xFFFFFF, "0"))
+button2.onTouch = function()
+  window:remove()
+  GUI.alert("Ключ активации:Pidoras)
+end
+
+local button3 = layout:addChild(GUI.button(1, 1, 8, 1, 0x4B4B4B, 0xFFFFFF, 0x0, 0xFFFFFF, "Exit"))
+button3.onTouch = function()
+  window:remove()
+  GUI.alert("Я обиделся")
+end
+
+workspace:draw()

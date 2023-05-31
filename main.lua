897799da5932bd612a3d0b662856019d5accc366
Michael Amudeshnikov <83592338+misha99fr@users.noreply.github.com>
2023-05-31 21:49:20 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,19 +1,19 @@
 local GUI = require("GUI")
 local system = require("System")
 
-local workspace, window, menu = system.addWindow(GUI.filledWindow(1, 1, 50, 16, 0x9966CC))
+local workspace, window, menu = system.addWindow(GUI.filledWindow(1, 1, 50, 16, 0xFF180153))
 
 local layout = window:addChild(GUI.layout(1, 1, window.width, window.height, 1, 1))
 
-local label = layout:addChild(GUI.label(1, 1, 25, 1, 0x0, "Mindows Setup"))
+local label = layout:addChild(GUI.label(1, 1, 25, 1, 0xFFFFFFFF, "Mindows Setup"))
 
-local button1 = layout:addChild(GUI.button(1, 1, 11, 1, 0x4B4B4B, 0xFFFFFF, 0x0, 0xFFFFFF, "1"))
+local button1 = layout:addChild(GUI.button(1, 1, 11, 1, 0x4B4B4B, 0xFFFFFF, 0x0, 0xFFFFFF, "Install"))
 button1.onTouch = function()
   window:remove()
   GUI.alert("АХАХАХАХХАХАХАХ, хер тебе а не Mindows 10")
 end
 
-local button2 = layout:addChild(GUI.button(1, 1, 8, 1, 0x4B4B4B, 0xFFFFFF, 0x0, 0xFFFFFF, "0"))
+local button2 = layout:addChild(GUI.button(1, 1, 8, 1, 0x4B4B4B, 0xFFFFFF, 0x0, 0xFFFFFF, "Help"))
 button2.onTouch = function()
   window:remove()
   GUI.alert("Ключ активации:Pidoras228")

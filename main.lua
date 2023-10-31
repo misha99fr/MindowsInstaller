b3962d225c87b0608a82e836d2bde6b209724fdf
Michael Amudeshnikov <83592338+misha99fr@users.noreply.github.com>
2023-10-31 16:18:53 +0300

    ДА ЧЕ Я СДЕЛАЛ НЕ ТАК БЛЯТЬ

--- main.lua
+++ main.lua
@@ -10,13 +10,14 @@ local label = layout:addChild(GUI.label(1, 1, 25, 1, 0xFFFFFFFF, "Mindows Setup"
 local button1 = layout:addChild(GUI.button(1, 1, 11, 1, 0x4B4B4B, 0xFFFFFF, 0x0, 0xFFFFFF, "Install"))
 button1.onTouch = function()
   window:remove()
-  GUI.alert("Sorry, Your PC Can't Run Mindows 11")
+  GUI.alert("To start setup, press OK")
+  GUI.alert("Enter product key here(Press ok to show dialog)")
 end
 
 local button2 = layout:addChild(GUI.button(1, 1, 8, 1, 0x4B4B4B, 0xFFFFFF, 0x0, 0xFFFFFF, "Help"))
 button2.onTouch = function()
   window:remove()
-  GUI.alert("Product Key: TGVBHIF")
+  GUI.alert("Requirments: MineOS-PC, all tier3-Components, ability to think, ability to install GBSoftware Products also product Key: OTIGF-XSFHGD-FGFGH")
 end
 
 local button3 = layout:addChild(GUI.button(1, 1, 8, 1, 0x4B4B4B, 0xFFFFFF, 0x0, 0xFFFFFF, "Exit"))
